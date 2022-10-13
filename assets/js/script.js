c0eed8ed0b2e29199230623a22403aee97058659
SanketPanchal <sanketp@altis.com.au>
2022-10-13 20:26:16 +1100

    city search function is implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 const APIKey = "f39d9467ef1aba260c6801b3458f906e";
+
+const searchFormEl = document.getElementById("city-search");
+const searchBtn = document.getElementById("search-btn");
+
+function citySearch(event) {
+    event.preventDefault();
+    $("#current-day-result").removeAttr("style");
+    $("#five-day-result").removeAttr("style");
+    clearCurrentResult();
+    searchHistoryStore();
+    returnResultsCurrentDay();
+    returnResultsFiveDay();
+    returnFiveDayDate();
+    pastCityRestore()
+    clickCounter++;
+    return;
+};
